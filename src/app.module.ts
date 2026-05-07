d5131fc416c80a7821dfe1c4885442a02e24ee7e
Dmitriy Kislitsyn <dima.music1989@gmail.com>
2026-05-07 11:52:30 +0300

    TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PropertyModule } from './property/property.module';
 import { ScheduleModule } from './schedule/schedule.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { pgConfig } from './dbConfig';
 
 @Module({
-  imports: [PropertyModule, ScheduleModule],
+  imports: [PropertyModule, ScheduleModule, TypeOrmModule.forRoot(pgConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/dbConfig.ts
@@ -0,0 +1,12 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { Property } from './property/entities/property.entity';
+
+export const pgConfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  port: 5430,
+  database: 'sacura_dev_data',
+  username: 'admin',
+  password: '123456',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true, // для production режима обязательно false иначе при, например, удалении столбца в таблице автоматически применятся изменения в базу данных и столбец с данными из нее удалиться безвозвратно
+};
